02a399258fe511f8d3ec7980a7cd2a581f77d676
jela3105 <jela3105@gmail.com>
2021-02-19 22:07:27 -0600

    Create bottom navigation bar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:qr_app/widges/custom_navigator_bar.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("History"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: Center(
         child: Text('HomePage'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
     );
   }
 }
