c50e80bc00a23256e0f71377b35df62ce9175905
jela3105 <jela3105@gmail.com>
2021-02-25 22:22:40 -0600

    Use scanListProvider to load scans

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'package:qr_app/providers/ui_provider.dart';
 import 'package:qr_app/providers/db_provider.dart';
+import 'package:qr_app/providers/scan_list_provider.dart';
 
 import 'package:qr_app/widges/custom_navigator_bar.dart';
 import 'package:qr_app/widges/scan_button.dart';
-
-import 'package:qr_app/providers/ui_provider.dart';
 
 import 'package:qr_app/src/pages/maps_page.dart';
 import 'package:qr_app/src/pages/addresses_page.dart';
@@ -42,12 +43,15 @@ class _HomePageBody extends StatelessWidget {
     //TODO: read database temporary
 
     final tempScan = ScanModel(value: 'http://google.com');
-    DBProvider.db.getAllScans().then(print);
-
+    //   DBProvider.db.getAllScans().then(print);
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: false);
     switch (currentIndex) {
       case 0:
+        scanListProvider.loadScansByType('geo');
         return MapsPage();
       case 1:
+        scanListProvider.loadScansByType('http');
         return AddressesPage();
       default:
         return MapsPage();
