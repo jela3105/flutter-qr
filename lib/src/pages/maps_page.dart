cfe87ceddd5f25f3e090cab4c464d23cf7eeed80
jela3105 <jela3105@gmail.com>
2021-02-25 22:13:55 -0600

    Show scans UI without database

--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("MapsPage"),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: Text('http://hello_world.com'),
+        subtitle: Text('ID: 1'),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        onTap: () => print('open someting...'),
+      ),
     );
   }
 }
