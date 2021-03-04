d1159ed8b8b683521e994cb82b206a10349730c3
jela3105 <jela3105@gmail.com>
2021-03-03 23:29:14 -0600

    Optimize addresses page and maps page

--- lib/src/pages/addresses_page.dart
+++ lib/src/pages/addresses_page.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:qr_app/providers/scan_list_provider.dart';
+import 'package:qr_app/widges/scan_tiles.dart';
 
 class AddressesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading:
-            Icon(Icons.home_outlined, color: Theme.of(context).primaryColor),
-        title: Text(scans[i].value),
-        subtitle: Text(scans[i].id.toString()),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        onTap: () => print('open someting...'),
-      ),
-    );
+    return ScanTiles(type: 'maps');
   }
 }
--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:qr_app/providers/scan_list_provider.dart';
+import 'package:qr_app/widges/scan_tiles.dart';
 
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => Dismissible(
-        key: UniqueKey(),
-        background: Container(color: Colors.red),
-        onDismissed: (DismissDirection direction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .deleteScanById(scans[i].id);
-        },
-        child: ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title: Text(scans[i].value),
-          subtitle: Text(scans[i].id.toString()),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print(scans[i].id),
-        ),
-      ),
-    );
+    return ScanTiles(type: 'maps');
   }
 }
