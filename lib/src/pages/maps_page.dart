db002981d5b098651a7282fc9d1fe7e48c876ab9
jela3105 <jela3105@gmail.com>
2021-03-03 23:20:51 -0600

    Delete item by id

--- lib/src/pages/maps_page.dart
+++ lib/src/pages/maps_page.dart
@@ -11,12 +11,20 @@ class MapsPage extends StatelessWidget {
 
     return ListView.builder(
       itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-        title: Text(scans[i].value),
-        subtitle: Text(scans[i].id.toString()),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        onTap: () => print(scans[i].id),
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(color: Colors.red),
+        onDismissed: (DismissDirection direction) {
+          Provider.of<ScanListProvider>(context, listen: false)
+              .deleteScanById(scans[i].id);
+        },
+        child: ListTile(
+          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+          title: Text(scans[i].value),
+          subtitle: Text(scans[i].id.toString()),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print(scans[i].id),
+        ),
       ),
     );
   }
